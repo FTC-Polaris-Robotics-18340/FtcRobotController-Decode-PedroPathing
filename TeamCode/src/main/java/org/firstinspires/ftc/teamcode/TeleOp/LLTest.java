975875bb1c0477656ec0348092b86a1d785f2f20
RidhimanPatwari <ridhiman.k.patwari@gmail.com>
2025-12-27 14:39:02 -0800

    returned distFromTag (wasn't in this branch for some reason)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/LLTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/LLTest.java
@@ -14,7 +14,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 public class LLTest extends OpMode {
 
     private Limelight3A limelight;
-
+    private double smoothedTa = 0.0;
+    private final double alpha = 0.3; // Smoothing factor
     private IMU imu;
 
     @Override
@@ -41,11 +42,28 @@ public class LLTest extends OpMode {
         LLResult llResult = limelight.getLatestResult();
         if (llResult != null && llResult.isValid()){
             Pose3D botPose = llResult.getBotpose_MT2();
+            double rawTa = llResult.getTa();
+            if (smoothedTa == 0.0) {
+                smoothedTa = rawTa; // Initialize on first valid reading
+            } else {
+                smoothedTa = alpha * rawTa + (1 - alpha) * smoothedTa; // Exponential smoothing
+            }
+
             telemetry.addData("Tx", llResult.getTx());
             telemetry.addData("Ty", llResult.getTy());
-            telemetry.addData("Ta", llResult.getTa());
+            telemetry.addData("rawTa", llResult.getTa());
+            telemetry.addData("smoothedTa", smoothedTa);
             telemetry.addData("Bot Pose", botPose.toString());
             telemetry.addData("Yaw", botPose.getOrientation().getYaw());
+            telemetry.addData("Distance from tag", getDistFromTag(smoothedTa));
+            telemetry.update();
         }
+    }
+
+    public double getDistFromTag(double ta) {
+        // eq: y = Ax^B
+        double A = 66.64891;
+        double B = -0.5320883;
+        return (A * Math.pow(ta, B));
     }
 }
