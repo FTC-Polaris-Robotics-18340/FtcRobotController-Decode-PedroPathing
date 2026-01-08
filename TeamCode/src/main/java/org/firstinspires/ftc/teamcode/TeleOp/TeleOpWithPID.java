d7c521b0f7eea79351463d7bba0865d27b70d664
anilk <komarrajufamily@gmial.com>
2026-01-07 21:15:01 -0800

    BasicDrive.java working, trying to consolidate all into TeleOpWithPID.java. current status and todos of documented via comment at top of this file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpWithPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpWithPID.java
@@ -8,6 +8,14 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.Servo;
 
+
+/*
+----------------- TASKS FOR THIS FILE --------------- || done?
+    - basic driving                                   || y
+    - intaking + shooting process                     || y
+    - shooter calc (equations w/ power & hood pos)    || TODO
+    - turret mechanism + calc (equation, ll, etc.)    || TODO
+*/
 @TeleOp(name="MainTeleOp")
 public class TeleOpWithPID extends LinearOpMode {
 
@@ -35,21 +43,36 @@ public class TeleOpWithPID extends LinearOpMode {
     private static final double KICK_REST = 0.0;
     private static final double KICK_FIRE = 1.0;
 
-    // drive stuff
-    robot = new Robot(hardwareMap);
-    drive = new MecanumDrive(robot.FrontLeft, robot.FrontRight, robot.BackLeft, robot.BackRight);
-    turretServo = new TurretServo(hardwareMap);
+    // basic drive
+    private DcMotor FL;
+    private DcMotor FR;
+    private DcMotor BL;
+    private DcMotor BR;
 
-    hoodServo = hardwareMap.get(Servo.class, "hood");
+    /*// not working drive stuff
+    public static Robot robot;
+    public static MecanumDrive drive;
+    public static GamepadEx gamepad1Ex;
+    public static GamepadEx gamepad2Ex;
+    public static TurretServo turretServo;
+    //Servo hoodServo; - already defined*/
 
-    gamepad1Ex = new GamepadEx(gamepad1);
-    gamepad2Ex = new GamepadEx(gamepad2);
+    /*private void HardwareStart() {
+        robot = new Robot(hardwareMap);
+        drive = new MecanumDrive(robot.FrontLeft, robot.FrontRight, robot.BackLeft, robot.BackRight);
+        turretServo = new TurretServo(hardwareMap);
+
+        hoodServo = hardwareMap.get(Servo.class, "hood");
+
+        gamepad1Ex = new GamepadEx(gamepad1);
+        gamepad2Ex = new GamepadEx(gamepad2);
+    }*/
 
     @Override
     public void runOpMode() {
 
         // ============== INTAKE & SHOOTER ===============
-        Intake  = hardwareMap.get(DcMotor.class, "intake");
+        Intake = hardwareMap.get(DcMotor.class, "intake");
 
         Shooter = hardwareMap.get(DcMotorEx.class, "shooter");
         Shooter.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -72,8 +95,16 @@ public class TeleOpWithPID extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
+            // ================ BASIC DRIVE =================
+            double lX = gamepad1.left_stick_x;
+            double lY = gamepad1.left_stick_y;
+            double rX = gamepad1.right_stick_x;
 
-            /* ===== Intake ===== */
+            drive(lY, lX, rX);
+
+            //===============================================
+
+            // ===== Intake =====
             if (gamepad1.left_trigger > 0.1 || gamepad1.right_trigger > 0.1) { // <- MAY CAUSE PROBLEMS
                 Intake.setPower(1.0);
                 Stopper.setPosition(STOPPER_OPEN);
@@ -82,14 +113,14 @@ public class TeleOpWithPID extends LinearOpMode {
                 Stopper.setPosition(STOPPER_BLOCK);
             }
 
-            /* ===== Shooter spin-up ===== */
+            // ===== Shooter spin-up =====
             if (gamepad1.right_trigger > 0.1) {
                 Shooter.setVelocity(TARGET_VELOCITY);
             } else {
                 Shooter.setVelocity(0);
             }
 
-            /* ===== Velocity check ===== */
+            // ===== Velocity check =====
             double currentVelocity = Shooter.getVelocity();
             double velocityError = Math.abs(TARGET_VELOCITY - currentVelocity);
 
@@ -100,14 +131,14 @@ public class TeleOpWithPID extends LinearOpMode {
                 shooterReady = false;
             }
 
-            /* ===== Kicker (only fires if shooter is ready) ===== */
+            // ===== Kicker (only fires if shooter is ready) =====
             if (gamepad1.left_bumper && shooterReady) {
                 Kicker.setPosition(KICK_FIRE);
             } else {
                 Kicker.setPosition(KICK_REST);
             }
 
-            /* ===== Telemetry ===== */
+            // ===== Telemetry =====
             telemetry.addData("Target Vel", TARGET_VELOCITY);
             telemetry.addData("Current Vel", "%.1f", currentVelocity);
             telemetry.addData("Error", "%.1f", velocityError);
@@ -115,8 +146,7 @@ public class TeleOpWithPID extends LinearOpMode {
             telemetry.update();
 
             // =======================================================
-
-            // ============= MECANUM DRIVE ======================
+            /*// ========= MECANUM DRIVE not working, use BasicDrive.java =========
             // previous-button states for simple edge detection
             boolean prevDpadUp = false;
             boolean prevDpadDown = false;
@@ -134,6 +164,36 @@ public class TeleOpWithPID extends LinearOpMode {
                 //double outPower = gamepad1.left_trigger;  // left trigger controls shooter power
 
                 //robot.shooter.setVelocityFromPower(outPower);
+
+             */
         }
+    }
+
+    public void drive(double forward, double right, double rotate) {
+        // This calculates the power needed for each wheel based on the amount of forward,
+        // strafe right, and rotate
+        double frontLeftPower = forward + right + rotate;
+        double frontRightPower = forward - right - rotate;
+        double backRightPower = forward + right - rotate;
+        double backLeftPower = forward - right + rotate;
+
+        double maxPower = 1.0;
+        double maxSpeed = 1.0;  // make this slower for outreaches
+
+        // This is needed to make sure we don't pass > 1.0 to any wheel
+        // It allows us to keep all of the motors in proportion to what they should
+        // be and not get clipped
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+
+        // We multiply by maxSpeed so that it can be set lower for outreaches
+        // When a young child is driving the robot, we may not want to allow full
+        // speed.
+        FL.setPower(maxSpeed * (frontLeftPower / maxPower));
+        FR.setPower(maxSpeed * (frontRightPower / maxPower));
+        BL.setPower(maxSpeed * (backLeftPower / maxPower));
+        BR.setPower(maxSpeed * (backRightPower / maxPower));
     }
 }
